8ed965e4a1eb47114451ef8aa1e970bc7bedec72
kidoto <kidotonenene@gmail.com>
2019-01-05 17:35:28 +0900

    test commit.

---
+++ _nuxt/5a96612439c2a6281fa5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{163:function(e,t,n){"use strict";n.r(t);var i=n(14),s=Object(i.a)({},function(){this.$createElement;this._self._c;return this._m(0)},[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("h1",[this._v(" header.")])])}],!1,null,null,null);s.options.__file="header.vue";t.default=s.exports}}]);
